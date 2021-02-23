b272d05c8d06067f583136352a87ed5fc4dc3061
Prasundas99 <prasundas115@gmail.com>
2021-02-24 01:21:23 +0530

    updated

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,14 @@
 import express from 'express';
 
-import{getPosts , createPosts} from '../controllers/posts.js';
+import{getPosts,  createPost, updatePost } from '../controllers/posts.js';
 
 const router = express.Router();
 
-router.get( '/' , getPosts );
-router.post( '/' , createPosts );
+router.get('/', getPosts);
+router.post('/', createPost);
+//router.get('/:id', getPosts);
+router.patch('/:id', updatePost);
+//router.delete('/:id', deletePost);
+//router.patch('/:id/likePost', likePost);
 
 export default router;
